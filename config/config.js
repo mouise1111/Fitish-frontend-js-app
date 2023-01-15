60c4f87c1504786ed2898ee7cfcf37db3f4e6262
Mouise Bashir <74969971+mouise1111@users.noreply.github.com>
2023-01-15 05:51:34 +0100

    added config file

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    "X-RapidAPI-Key": "b7f0fd548fmsh74bc54380cef7e6p19c412jsn2378f639d421",
+  },
+};
+
+export default options;
